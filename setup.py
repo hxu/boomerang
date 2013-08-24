b5332d2d112e6bce10d5a1b9e8f119b6d5513b2d
Han Xu <hgcrpd@gmail.com>
2013-08-24 18:35:50 +0800

    scaffolding

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="Boomerang",
+    version='0.1.0',
+    url='https://github.com/hxu/boomerang',
+    packages=['boomerang',],
+    license='MIT',
+    install_requires=[
+        'boto',
+    ]
+)
