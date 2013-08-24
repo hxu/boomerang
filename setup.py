772a10295ddd81b3078ff9d2afa6a741ab6ead83
Han Xu <hgcrpd@gmail.com>
2013-08-24 18:49:28 +0800

    ...

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+README = open('README.rst').read()
 
 setup(
     name="Boomerang",
     version='0.1.0',
+    packages=find_packages(),
+    description='Boomerang is a toolset for dispatching discrete R processing jobs to Amazon AWS'
+                'services then automatically returning the results and tearing down the AWS resources',
     url='https://github.com/hxu/boomerang',
-    packages=['boomerang',],
     license='MIT',
+    author='Han Xu',
+    long_description=README,
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License (MIT)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: System :: Distributed Computing',
+    ],
     install_requires=[
         'boto',
     ]
