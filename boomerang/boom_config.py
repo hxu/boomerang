1cb63a3c76c7de1a86dd97e84bc4be329bcdcc87
Han Xu <hgcrpd@gmail.com>
2013-08-26 23:28:51 +0800

    stray comma

--- boomerang/boom_config.py
+++ boomerang/boom_config.py
@@ -14,4 +14,4 @@ if os.path.exists(DEFAULT_CONFIG_PATH):
     if cfg_file.has_option('aws_credentials', 'aws_access_key_id'):
         AWS_ACCESS_KEY_ID = cfg_file.get('aws_credentials', 'aws_access_key_id')
     if cfg_file.has_option('aws_credentials', 'aws_secret_access_key'):
-        AWS_SECRET_ACCESS_KEY = cfg_file.get('aws_credentials', 'aws_secret_acces_key')
+        AWS_SECRET_ACCESS_KEY = cfg_file.get('aws_credentials', 'aws_secret_access_key')
--- boomerang/connection.py
+++ boomerang/connection.py
@@ -8,15 +8,15 @@ import boto
 
 def connect_ec2(aws_access_key_id=None, aws_secret_access_key=None):
     if aws_access_key_id is None:
-        aws_access_key_id = boom_config.AWS_ACCESS_KEY_ID,
+        aws_access_key_id = boom_config.AWS_ACCESS_KEY_ID
     if aws_secret_access_key is None:
         aws_secret_access_key = boom_config.AWS_SECRET_ACCESS_KEY
-    return boto.connect_ec2(aws_access_key_id, aws_secret_access_key)
+    return boto.connect_ec2(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
 
 def connect_s3(aws_access_key_id=None, aws_secret_access_key=None):
     if aws_access_key_id is None:
-        aws_access_key_id = boom_config.AWS_ACCESS_KEY_ID,
+        aws_access_key_id = boom_config.AWS_ACCESS_KEY_ID
     if aws_secret_access_key is None:
         aws_secret_access_key = boom_config.AWS_SECRET_ACCESS_KEY
-    return boto.connect_s3(aws_access_key_id, aws_secret_access_key)
+    return boto.connect_s3(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
