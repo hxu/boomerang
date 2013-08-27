168ff2badcc90c2b9d0393a9abd325384e62ff21
Han Xu <hgcrpd@gmail.com>
2013-08-27 10:46:49 +0800

    more config tweaks

--- boomerang/boom_config.py
+++ boomerang/boom_config.py
@@ -8,6 +8,21 @@ DEFAULT_CONFIG_PATH = os.path.expanduser('~/.boomerang')
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
 
+DEFAULT_REGION = 'us-east-1'
+
+DEFAULT_INSTANCE = 't1.micro'
+DEFAULT_AMI = 'ami-0b9ad862'
+DEFAULT_SECURITY_GROUP = ''
+
+DEFAULT_SSH_KEY = ''
+DEFAULT_SSH_KEY_PATH = ''
+
+DEFAULT_BUCKET = ''
+
+TEMPORARY_FOLDER = '.boom_tmp/'
+
+
+# Load the user config and overwrite objects in this module with the values in that file
 if os.path.exists(DEFAULT_CONFIG_PATH):
     cfg_file = imp.load_source('user_config', DEFAULT_CONFIG_PATH)
     this_module = sys.modules[__name__]
---
+++ boomerang/templates/example_config.py
@@ -0,0 +1,18 @@
+########
+# Example Configuration File
+########
+
+AWS_ACCESS_KEY_ID = '<YOUR ACCESS KEY>'
+AWS_SECRET_ACCESS_KEY = '<YOUR SECRET KEY>'
+DEFAULT_REGION = 'us-east-1'
+
+DEFAULT_INSTANCE = 't1.micro'
+DEFAULT_AMI = 'ami-0b9ad862'
+DEFAULT_SECURITY_GROUP = 'ssh-only'
+
+DEFAULT_SSH_KEY = 'hgcrpd'
+DEFAULT_SSH_KEY_PATH = '~/aws/hgcrpd.pem'
+
+DEFAULT_BUCKET = 'boom_test'
+
+TEMPORARY_FOLDER = '.boom_tmp/'
